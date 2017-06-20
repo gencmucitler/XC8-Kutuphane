77b22240762cd6efb1cf006a8e2f9f87fedad0ac
gencmucitler <sigmoid34@gmail.com>
2017-06-20 18:59:06 +0300

    PIC'in milisaniye cinsinden çalışma süresi
    PIC'in milisaniye cinsinden çalışma süresi

---
+++ Zaman/zaman.h
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Kütüphane     : miliSaniye                                                  *
+ * Yazar         : sigmoid                                                     *
+ * Web           : http://www.gencmucitler.com                                 *
+ * Baþlangýç     : 18 Haziran 2017                                             *
+ * Versiyon      : 0.1                                                         *
+ *                                                                             *
+ * PIC kaç miliSaniyedir enerjili olduðunu sayar. Yaklaþýk 50 gün sonra sayaç  * 
+ * sýfýrlanýr.                                                                 * 
+ ******************************************************************************/
+
+#ifndef ZAMAN_H
+#define	ZAMAN_H
+
+//fonksiyon prototipleri
+void _miliSaat(void);
+unsigned long miliSaniye(void);
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* ZAMAN_H */
+
