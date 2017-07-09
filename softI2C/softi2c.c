7eefc836d313ba2da1e03f0f3523dc4a668f3f5c
gencmucitler <sigmoid34@gmail.com>
2017-07-09 17:04:20 +0300

    Yazılımsal I2C Kütüphanesi
    Yazılımsal I2C Kütüphanesi

---
+++ softI2C/softi2c.c
@@ -0,0 +1,130 @@
+/**********************************************************
+*  Yazar 	: sigmoid
+*  Web 		: http://www.gencmucitler.com
+*  İlk		: Temmuz 2010
+*  Düzenleme: Yok 
+*  Versiyon : 0.1
+*  Açıklama :  software i2c master mode
+*  100KHz bağlantı hızı..
+***********************************************************/
+#include "mcc_generated_files/mcc.h"
+#include "softi2c.h"
+
+
+
+//diğer fonksiyonlar çalıştırılmadan önce ilk olarak bu çalıştırılmalı
+void softi2c_kur(void)
+{
+	SOFT_SCL=1;
+	SOFT_SDA=1;
+	SOFT_SCL_TRIS=0;	//çıkış
+	SOFT_SDA_TRIS=1;	//high empedans - giriş
+}	
+
+//start sinyalini gönderir
+void softi2c_baslat(void)
+{
+	SOFT_SCL=1;
+	__delay_us(4);
+	SOFT_SDA_TRIS=0;	//çıkış
+	SOFT_SDA=0;	// Clock high konumda iken 1 den 0 a düş.
+	__delay_us(4);
+	SOFT_SCL=0;
+	SOFT_SDA_TRIS=1;	//giriş	
+}
+	
+//stop sinyalini gönderir
+void softi2c_durdur(void)
+{
+	SOFT_SDA_TRIS=0;
+	SOFT_SDA=0;
+	SOFT_SCL=1;
+	__delay_us(4);
+	SOFT_SDA_TRIS=1;	//Clock high konumunda iken 0 dan 1e çık.
+	__delay_us(4);
+}
+
+//restart sinyalini gönderir	
+void softi2c_yenidenbaslat(void)
+{
+	SOFT_SCL=1;
+	__delay_us(4);
+	SOFT_SDA_TRIS=0;	//çıkış
+	SOFT_SDA=0;	// Clock high konumda iken 1 den 0 a düş.
+	__delay_us(4);
+	SOFT_SCL=0;
+	SOFT_SDA_TRIS=1;	//giriş	
+}
+
+//1 byte veri gönderir, ack sinyalini okur
+char softi2c_yaz(char veri)
+{
+	char i,temp;
+	SOFT_SCL=0;
+	for(i=8;i>0;i--)
+	{
+		veri=veri<<1;
+		if(CARRY==0)
+		{
+			SOFT_SDA_TRIS=0;
+			SOFT_SDA=0;
+		}	
+		else
+		{
+			SOFT_SDA_TRIS=1;
+			SOFT_SDA=1;
+		}	
+		SOFT_SCL=1;
+		__delay_us(4);
+		SOFT_SCL=0;
+		SOFT_SDA_TRIS=1;
+	}	
+	//0 dönerse ack alınmış demektir.
+		SOFT_SCL=0;
+		SOFT_SDA_TRIS=1;
+		__delay_us(4);
+		SOFT_SCL=1;
+		temp=SOFT_SDA;	//ack değerini al.
+		SOFT_SCL=0;
+		
+		return temp;
+		
+}
+
+//1 byte veri okur, ack sinyali gönderir.
+char softi2c_oku(char onay)
+{
+	char i,veri=0;
+	SOFT_SCL=0;
+	SOFT_SDA_TRIS=1;
+	for(i=8;i>0;i--)
+	{
+		veri=veri<<1;
+		SOFT_SCL=1;
+		__delay_us(4);
+		if(SOFT_SDA==1)
+		{
+			veri= veri | 0x01;
+		}	
+		SOFT_SCL=0;
+        __delay_us(2);	
+	}	
+	//onay kodunu gönder..
+	if(onay==0)
+	{
+		SOFT_SDA_TRIS=0;
+		SOFT_SDA=0;
+	}	
+	else
+	{
+		SOFT_SDA_TRIS=1;
+		SOFT_SDA=1;
+	}	
+	SOFT_SCL=1;
+	__delay_us(4);
+	SOFT_SCL=0;
+	SOFT_SDA_TRIS=1;
+	
+	return veri;
+}
+	
