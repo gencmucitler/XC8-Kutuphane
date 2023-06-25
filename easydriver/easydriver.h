a81173533ad3224ee82b2a28ea3b4398aa2d2baf
gencmucitler <sigmoid34@gmail.com>
2023-06-25 20:50:02 +0300

    Update easydriver.h

--- easydriver/easydriver.h
+++ easydriver/easydriver.h
@@ -1,30 +1,29 @@
 /*******************************************************************************
- * Kütüphane     : Easydriver Adım motor kütüphanesi                           *
+ * KÃ¼tÃ¼phane     : Easydriver AdÄ±m motor kÃ¼tÃ¼phanesi                           *
  * Yazar         : sigmoid                                                     *
- * Web           : http://www.gencmucitler.com                                 *
- * Başlangıç     : 4 Ocak 2018                                                 *
+ * BaÅŸlangÄ±Ã§     : 4 Ocak 2018                                                 *
  * Versiyon      : 0.2                                                         *
  *                                                                             *
  ******************************************************************************/
 
-//pin tanımlamaları
+//pin tanÄ±mlamalarÄ±
 #define step_DIR   LATB0
 #define step_PULSE LATB1
 
-//motor 360 derecelik tam turunu kaç adımda tamamlıyor8ı
+//motor 360 derecelik tam turunu kaÃ§ adÄ±mda tamamlÄ±yor8Ä±
 #define motorAdimi  1600
 
-//motor yönü
+//motor yÃ¶nÃ¼
 #define saatYonunde     0
 #define saatTersYonunde 1
 #define CW              0
 #define CCW             1
 
-//Hız ayarları - Hz olarak girilmeli
-#define _maksimum_hiz    16000  //1 saniyedeki maksimum adım sayısı 
-#define _minumum_hiz     800    //1 saniyedeki minumum adım sayısı 
-#define _hizlanmaadimi   4    //kaç adim da bir hızlanacak
-#define _yavaslamaadimi  10    //kaç adim da bir yavaşlayacak
+//HÄ±z ayarlarÄ± - Hz olarak girilmeli
+#define _maksimum_hiz    16000  //1 saniyedeki maksimum adÄ±m sayÄ±sÄ± 
+#define _minumum_hiz     800    //1 saniyedeki minumum adÄ±m sayÄ±sÄ± 
+#define _hizlanmaadimi   4    //kaÃ§ adim da bir hÄ±zlanacak
+#define _yavaslamaadimi  10    //kaÃ§ adim da bir yavaÅŸlayacak
 
 #define _sure          100000UL
 
