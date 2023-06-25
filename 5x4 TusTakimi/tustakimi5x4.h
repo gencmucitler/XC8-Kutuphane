df07bb4766ea15de8b2314fbb55e1c981f2c1e5c
gencmucitler <sigmoid34@gmail.com>
2023-06-25 20:48:15 +0300

    Update tustakimi5x4.h

--- 5x4 TusTakimi/tustakimi5x4.h
+++ 5x4 TusTakimi/tustakimi5x4.h
@@ -1,56 +1,55 @@
 /*******************************************************************************
- * Kütüphane     : 5x4 Tuş Takımı Okuma Kütüphanesi                            *
+ * KÃ¼tÃ¼phane     : 5x4 TuÅŸ TakÄ±mÄ± Okuma KÃ¼tÃ¼phanesi                            *
  * Yazar         : sigmoid                                                     *
- * Web           : http://www.gencmucitler.com                                 *
- * Başlangıç     : 18 Haziran 2017                                             *
+ * BaÅŸlangÄ±Ã§     : 18 Haziran 2017                                             *
  * Versiyon      : 0.1                                                         *
  *                                                                             *
- * 4x4 matriks tuş takımı kütüphanesi                                          *
+ * 4x4 matriks tuÅŸ takÄ±mÄ± kÃ¼tÃ¼phanesi                                          *
  ******************************************************************************/
 
-//SATIRLAR çıkış, SUTUNLAR giriş yapılmalı.  Giriş portları pull-up direnci 
-//takılmalı veya internal pull-up aktif edilmelidir.
+//SATIRLAR Ã§Ä±kÄ±ÅŸ, SUTUNLAR giriÅŸ yapÄ±lmalÄ±.  GiriÅŸ portlarÄ± pull-up direnci 
+//takÄ±lmalÄ± veya internal pull-up aktif edilmelidir.
 
-#define SATIR1  LATCbits.LATC7        //satırlar
+#define SATIR1  LATCbits.LATC7        //satÄ±rlar
 #define SATIR2  LATDbits.LATD4
 #define SATIR3  LATDbits.LATD5
 #define SATIR4  LATDbits.LATD6
 #define SATIR5  LATDbits.LATD7
 
-#define SUTUN1  PORTBbits.RB3        //sütunlar
+#define SUTUN1  PORTBbits.RB3        //sÃ¼tunlar
 #define SUTUN2  PORTBbits.RB2
 #define SUTUN3  PORTBbits.RB1
 #define SUTUN4  PORTBbits.RB0
 
 
-// tuş takımı patterni
+// tuÅŸ takÄ±mÄ± patterni
 const char tuslar[]={   'Q','W','#','*',    //F1   F2  #    *   
-                        '1','2','3','Y',    //1    2   3     Yukarı
-                        '4','5','6','A',    //4    5   6     Aşağı
-                        '7','8','9','C',    //7    8   9     Çıkış /ESC
-                        'L','0','R','G'     //Left 0   Right Giriş/Enter
+                        '1','2','3','Y',    //1    2   3     YukarÄ±
+                        '4','5','6','A',    //4    5   6     AÅŸaÄŸÄ±
+                        '7','8','9','C',    //7    8   9     Ã‡Ä±kÄ±ÅŸ /ESC
+                        'L','0','R','G'     //Left 0   Right GiriÅŸ/Enter
 };
 
 #define debounce_suresi 20u         // 20ms
-#define cift_tiklama_suresi 250u    // iki kez tıklama için gereken maksimum süre.
-#define kisa_basma_suresi   300u    // kısa basma süresi çift tıklama süresinden 
+#define cift_tiklama_suresi 250u    // iki kez tÄ±klama iÃ§in gereken maksimum sÃ¼re.
+#define kisa_basma_suresi   300u    // kÄ±sa basma sÃ¼resi Ã§ift tÄ±klama sÃ¼resinden 
                                     // uzun girilmeli.
 #define uzun_basma_suresi   1500u   //2saniye
 
 typedef enum _buton_durum_e
 {
-    SERBEST=0,          //buton basılmadığında boşta ise.
-    DEBOUNCE,           //buton basılma anındaki kontak sıcraması(debounce) testi için
-    BASILDI,            //1 kez çalışır
-    BASILI_TUTULUYOR,   //butona basılıyor
-    CIFT_TIKLAMA,       //iki kez peş peşe butona basılıp bırakılınca tetiklenir.
-    KISA_BASILDI,       //butona kısa bir süre basılınca 1 kez tetiklenir.
-    UZUN_BASILDI,       //buton belirtilen süre boyunca basılırsa 1 kez çalışır.
-    DEBOUNCE2,          //buton bırakılma anındaki kontak sıcraması(debounce) testi için
-    BIRAKILDI           //1 kez çalışır.
+    SERBEST=0,          //buton basÄ±lmadÄ±ÄŸÄ±nda boÅŸta ise.
+    DEBOUNCE,           //buton basÄ±lma anÄ±ndaki kontak sÄ±cramasÄ±(debounce) testi iÃ§in
+    BASILDI,            //1 kez Ã§alÄ±ÅŸÄ±r
+    BASILI_TUTULUYOR,   //butona basÄ±lÄ±yor
+    CIFT_TIKLAMA,       //iki kez peÅŸ peÅŸe butona basÄ±lÄ±p bÄ±rakÄ±lÄ±nca tetiklenir.
+    KISA_BASILDI,       //butona kÄ±sa bir sÃ¼re basÄ±lÄ±nca 1 kez tetiklenir.
+    UZUN_BASILDI,       //buton belirtilen sÃ¼re boyunca basÄ±lÄ±rsa 1 kez Ã§alÄ±ÅŸÄ±r.
+    DEBOUNCE2,          //buton bÄ±rakÄ±lma anÄ±ndaki kontak sÄ±cramasÄ±(debounce) testi iÃ§in
+    BIRAKILDI           //1 kez Ã§alÄ±ÅŸÄ±r.
 }buton_durum_e;
 
-//Değişken Tanımlamaları
+//DeÄŸiÅŸken TanÄ±mlamalarÄ±
 buton_durum_e butonDurumu=SERBEST;
 
 //fonksiyon prototipleri
