978770995b90aa47eb489a9b35b6a28fd94b1fba
gencmucitler <sigmoid34@gmail.com>
2023-06-25 21:00:52 +0300

    Update step28byj-48.h

--- Ornekler/step28byj-48.X/step28byj-48.h
+++ Ornekler/step28byj-48.X/step28byj-48.h
@@ -1,26 +1,25 @@
 /*******************************************************************************
- * Kütüphane     : 28BYJ-48 Adým motor kütüphanesi                             *
+ * KÃ¼tÃ¼phane     : 28BYJ-48 AdÃ½m motor kÃ¼tÃ¼phanesi                             *
  * Yazar         : sigmoid                                                     *
- * Web           : http://www.gencmucitler.com                                 *
- * Baþlangýç     : 17 Aralýk  2017                                             *
+ * BaÃ¾langÃ½Ã§     : 17 AralÃ½k  2017                                             *
  * Versiyon      : 0.1                                                         *
  *                                                                             *
  ******************************************************************************/
 
-//pin tanýmlamalarý
+//pin tanÃ½mlamalarÃ½
 #define motorPin_Turuncu    LATB0
 #define motorPin_Sari       LATB1
 #define motorPin_Pembe      LATB2
 #define motorPin_Mavi       LATB3
 
-//motor yönü
+//motor yÃ¶nÃ¼
 #define saatYonunde     0
 #define saatTersYonunde 1
 #define CW              0
 #define CCW             1
 
 
-//motorun 360 derecelikönüþ için gereken adým sayýsý
+//motorun 360 derecelikÃ¶nÃ¼Ã¾ iÃ§in gereken adÃ½m sayÃ½sÃ½
 #define motorAdimi  4096
 
 //fonksiyon prototipleri
