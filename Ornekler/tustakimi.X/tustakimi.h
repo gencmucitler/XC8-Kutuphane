6bad4d0be4d6f9476b94d2a7147fd5609ca03e2b
gencmucitler <sigmoid34@gmail.com>
2023-06-25 21:01:28 +0300

    Update tustakimi.h

--- Ornekler/tustakimi.X/tustakimi.h
+++ Ornekler/tustakimi.X/tustakimi.h
@@ -1,29 +1,28 @@
 /*******************************************************************************
- * Kütüphane     : 4x4 Tuş Takımı Okuma Kütüphanesi                            *
+ * KÃ¼tÃ¼phane     : 4x4 TuÅŸ TakÄ±mÄ± Okuma KÃ¼tÃ¼phanesi                            *
  * Yazar         : sigmoid                                                     *
- * Web           : http://www.gencmucitler.com                                 *
- * Başlangıç     : 18 Haziran 2017                                             *
+ * BaÅŸlangÄ±Ã§     : 18 Haziran 2017                                             *
  * Versiyon      : 0.1                                                         *
  *                                                                             *
- * 4x4 matriks tuş takımı kütüphanesi                                          *
+ * 4x4 matriks tuÅŸ takÄ±mÄ± kÃ¼tÃ¼phanesi                                          *
  ******************************************************************************/
 
-//Portun 0..3 çıkış 4..7 giriş yapılmalı. Kısaca Satırlar çıkış, sutünlar giriş 
-//yapılmalı. Giriş portları pull-up direnci takılmalı veya internal pull-up 
+//Portun 0..3 Ã§Ä±kÄ±ÅŸ 4..7 giriÅŸ yapÄ±lmalÄ±. KÄ±saca SatÄ±rlar Ã§Ä±kÄ±ÅŸ, sutÃ¼nlar giriÅŸ 
+//yapÄ±lmalÄ±. GiriÅŸ portlarÄ± pull-up direnci takÄ±lmalÄ± veya internal pull-up 
 //aktif edilmelidir.
 
-#define SATIR1  LATBbits.LATB0        //satırlar
+#define SATIR1  LATBbits.LATB0        //satÄ±rlar
 #define SATIR2  LATBbits.LATB1
 #define SATIR3  LATBbits.LATB2
 #define SATIR4  LATBbits.LATB3
 
-#define SUTUN1  PORTBbits.RB4        //sütunlar
+#define SUTUN1  PORTBbits.RB4        //sÃ¼tunlar
 #define SUTUN2  PORTBbits.RB5
 #define SUTUN3  PORTBbits.RB6
 #define SUTUN4  PORTBbits.RB7
 
 
-// tuş takımı patterni
+// tuÅŸ takÄ±mÄ± patterni
 const char tuslar[]={   '1','2','3','A',
                         '4','5','6','B',
                         '7','8','9','C',
@@ -31,25 +30,25 @@ const char tuslar[]={   '1','2','3','A',
 };
 
 #define debounce_suresi 20u         // 20ms
-#define cift_tiklama_suresi 250u    // iki kez tıklama için gereken maksimum süre.
-#define kisa_basma_suresi   300u    // kısa basma süresi çift tıklama süresinden 
+#define cift_tiklama_suresi 250u    // iki kez tÄ±klama iÃ§in gereken maksimum sÃ¼re.
+#define kisa_basma_suresi   300u    // kÄ±sa basma sÃ¼resi Ã§ift tÄ±klama sÃ¼resinden 
                                     // uzun girilmeli.
 #define uzun_basma_suresi   1500u   //2saniye
 
 typedef enum _buton_durum_e
 {
-    SERBEST=0,          //buton basılmadığında boşta ise.
-    DEBOUNCE,           //buton basılma anındaki kontak sıcraması(debounce) testi için
-    BASILDI,            //1 kez çalışır
-    BASILI_TUTULUYOR,   //butona basılıyor
-    CIFT_TIKLAMA,       //iki kez peş peşe butona basılıp bırakılınca tetiklenir.
-    KISA_BASILDI,       //butona kısa bir süre basılınca 1 kez tetiklenir.
-    UZUN_BASILDI,       //buton belirtilen süre boyunca basılırsa 1 kez çalışır.
-    DEBOUNCE2,          //buton bırakılma anındaki kontak sıcraması(debounce) testi için
-    BIRAKILDI           //1 kez çalışır.
+    SERBEST=0,          //buton basÄ±lmadÄ±ÄŸÄ±nda boÅŸta ise.
+    DEBOUNCE,           //buton basÄ±lma anÄ±ndaki kontak sÄ±cramasÄ±(debounce) testi iÃ§in
+    BASILDI,            //1 kez Ã§alÄ±ÅŸÄ±r
+    BASILI_TUTULUYOR,   //butona basÄ±lÄ±yor
+    CIFT_TIKLAMA,       //iki kez peÅŸ peÅŸe butona basÄ±lÄ±p bÄ±rakÄ±lÄ±nca tetiklenir.
+    KISA_BASILDI,       //butona kÄ±sa bir sÃ¼re basÄ±lÄ±nca 1 kez tetiklenir.
+    UZUN_BASILDI,       //buton belirtilen sÃ¼re boyunca basÄ±lÄ±rsa 1 kez Ã§alÄ±ÅŸÄ±r.
+    DEBOUNCE2,          //buton bÄ±rakÄ±lma anÄ±ndaki kontak sÄ±cramasÄ±(debounce) testi iÃ§in
+    BIRAKILDI           //1 kez Ã§alÄ±ÅŸÄ±r.
 }buton_durum_e;
 
-//Değişken Tanımlamaları
+//DeÄŸiÅŸken TanÄ±mlamalarÄ±
 buton_durum_e butonDurumu=SERBEST;
 
 //fonksiyon prototipleri
